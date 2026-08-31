2bed2c12e9e347eedd680dd25819488e78585dca
Viper Davis <viperxc2204@gmail.com>
2026-08-31 14:30:39 +0100

    feat: initialize core magic systems and server regeneration

--- src/main/java/com/viperxc/wizardsmayham/WizardsMayham.java
+++ src/main/java/com/viperxc/wizardsmayham/WizardsMayham.java
@@ -1,6 +1,11 @@
 package com.viperxc.wizardsmayham;
 
+import com.viperxc.wizardsmayham.item.ModItems;
+import com.viperxc.wizardsmayham.magic.MagicDataStore;
+import com.viperxc.wizardsmayham.magic.SpellRegistry;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,6 +15,20 @@ public final class WizardsMayham implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        LOGGER.info("Wizards and Mayham is initializing...");
+        LOGGER.info("Initializing Wizards and Mayham core...");
+        ModItems.initialize();
+        SpellRegistry.initialize();
+
+        ServerTickEvents.END_SERVER_TICK.register(server -> {
+            if (server.getTickCount() % 20 != 0) return;
+            MagicDataStore store = MagicDataStore.get(server);
+            server.getPlayerList().getPlayers().forEach(player -> store.get(player.getUUID()).regen());
+            store.markDirty();
+        });
+
+        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
+            MagicDataStore.get(server).get(handler.getPlayer().getUUID());
+            MagicDataStore.get(server).markDirty();
+        });
     }
 }
